3e8634aef5f204e297f6e2ded41e2906f3ac22fc
feichay10 <alu0101364544@ull.edu.es>
2023-11-28 00:10:25 +0000

    Adding histograma.cu

---
+++ Modulo_II_Programacion_GPUs/histograma.cu
@@ -0,0 +1,17 @@
+/**
+ *
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Arquitecturas Avanzadas y de Propósito Específico
+ * Curso: 4º
+ * @file histograma.cu
+ * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
+ * @brief 
+ * @version 0.1
+ * 
+ * Compilar y ejecutar con: nvcc histograma.cu -o histograma
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */

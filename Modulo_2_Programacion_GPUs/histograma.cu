bf369127d56999b88d40a3dd3375b3eef70b3480
feichay10 <alu0101364544@ull.edu.es>
2024-05-27 13:25:56 +0100

    minor fixes

--- Modulo_2_Programacion_GPUs/histograma.cu
+++ Modulo_2_Programacion_GPUs/histograma.cu
@@ -27,14 +27,10 @@
 
 #include <iostream>
 
-#define N 500000000  // Numero de elementos en el vector V
-#define M 8          // Numero de cajas en el histograma
+#define N 500000000    // Numero de elementos en el vector V
+#define M 8            // Numero de elementos o cajas en el histograma
+#define REPETITIONS 20 // Numero de repeticiones para el calculo de la media
 
-#define REPETITIONS 20  // Repeticion de pruevas para calculo de media, max y min
-#define SCALA 50            // Datos calculados en cada hilo
-
-__device__ int vector_V[N];  // Vector de datos de entrada
-__device__ int vector_H[M];  // Vector del histograma, "cajas"
 
 /**
  * @brief Funcion para la comprobación de errores de CUDA
@@ -44,119 +40,76 @@ static void CheckCudaErrorAux(const char *, unsigned, const char *, cudaError_t)
 #define CUDA_CHECK_RETURN(value) CheckCudaErrorAux(__FILE__, __LINE__, #value, value)
 
 /**
- * @brief Kernel para la inicializacion del vector de datos de entrada
- * 
- */
-__global__ void kernel_init_vector(int random, curandState *states, int threadsPerBlock, int blocksPerGrid) {
-  int iterations = SCALA;
-  if (blocksPerGrid - 1 == blockIdx.x && threadIdx.x == threadsPerBlock - 1) {
-    iterations = iterations + (N % SCALA);
-  }
-  unsigned id_x = blockIdx.x * blockDim.x + threadIdx.x;
-  curandState *state = states + id_x;
-
-  curand_init(random, id_x, 0, state);
-  for (int i = 0; i < iterations; i++) {
-    if (id_x * SCALA + i < N) {
-      vector_V[id_x * SCALA + i] = (int)((curand_uniform(state) * 1000)) % M;
-    }
-  }
-}
-
-/**
- * @brief Kernel para la inicializacion del vector del histograma
- * 
- */
-__global__ void kernel_init_histogram() {
-  int id_x = blockIdx.x * blockDim.x + threadIdx.x;
-  if (id_x < M) {
-    vector_H[id_x] = 0;
-  }
-}
-
-/**
  * @brief Kernel para el calculo del histograma
+ * 
+ */
+__global__ void kernel(int *vector, int *histogram) {
+  int id_x = threadIdx.x + blockIdx.x * blockDim.x;
+  int pos_histogram = 0;
+  if (id_x < N) {
+    pos_histogram = vector[id_x] % M; // ValorElementoV mod M
+    atomicAdd(&histogram[pos_histogram], 1);
+  }
+}
+
+/**
+ * @brief Kernel para la inicializacion del histograma
  * 
  * @param threadsPerBlock 
  * @param blocksPerGrid 
  * @return __global__ 
  */
-__global__ void histogram(int threadsPerBlock, int blocksPerGrid) {
-  int id_x = blockIdx.x * blockDim.x + threadIdx.x;
-  int iterations = SCALA;
-  if (blocksPerGrid - 1 == blockIdx.x && threadIdx.x == threadsPerBlock - 1) {
-    iterations = iterations + (N % SCALA);
-  }
-  for (int i = 0; i < iterations; i++) {
-    if (id_x * SCALA + i < N) {
-      atomicAdd(&vector_H[vector_V[id_x * SCALA + i]], 1);
+__global__ void histogram(int *histogram) {
+  int id_x = threadIdx.x + blockIdx.x * blockDim.x;
+  if (id_x < N) {
+    if (id_x == 0) {
+      for (int i = 0; i < M; i++) {
+        histogram[i] = 0;
+      }
     }
   }
 }
 
+
 int main() {
-  // Valores para la inicializacion del vector de datos de entrada
-  int random = time(NULL);
-  static curandState *states;
+  // Realizar un histograma de un vector V de un número elevado N de elementos
+  // enteros aleatorios. El histograma consiste en un vector H que tiene M
+  // elementos que representan "cajas".
 
-  int h_vector_H[M];  // Vector del histograma en el host
-  int threadsPerBlock = 1024; // Hilos por bloque
-  int blocksPerGrid = ((N / SCALA) + threadsPerBlock - 1) / threadsPerBlock;
-  float t_duration[REPETITIONS]; // Vector para guardar los tiempos de ejecucion
+  // En cada caja se cuenta el número de veces que ha aparecido un elemento del
+  // vector V con el valor adecuado para asignarlo a esa caja (normalmente cada
+  // caja representa un rango o intervalo de valores). En nuestro caso, para
+  // simplificar la asignación del elemento de V a su caja correspondiente del
+  // histograma, vamos a realizar la operación ValorElementoV Módulo M, que nos
+  // da directamente el índice de la caja del histograma a la que pertenecerá
+  // ese elemento y cuyo contenido deberemos incrementar. Se sugiere como N un
+  // valor del orden de millones de elementos y como M, 8 cajas.
 
-  // Variables para el calculo de tiempo
-  cudaEvent_t start,stop;
-  cudaEventCreate(&start);
-  cudaEventCreate(&stop);
+  // Como implementación base (que podremos mejorar en tiempo o no) se pide
+  // crear tantos hilos como elementos de V para que cada uno se encargue de ir
+  // al elemento que le corresponda en V e incremente la caja correcta en el
+  // vector histograma H (posiblemente de forma atómica).
 
-  for (int i = 0; i < REPETITIONS; i++) {
-    CUDA_CHECK_RETURN(cudaEventRecord(start, 0));
+  // Inicializacion de los vectores
+  int *h_vector = (int *)malloc(N * sizeof(int));
+  int *h_histogram = (int *)malloc(M * sizeof(int));
 
-    CUDA_CHECK_RETURN(cudaMalloc((void **)&states, blocksPerGrid * threadsPerBlock * sizeof(curandState)));
-    kernel_init_vector<<<blocksPerGrid, threadsPerBlock>>>(random, states, threadsPerBlock, blocksPerGrid);
-    CUDA_CHECK_RETURN(cudaGetLastError());
-    CUDA_CHECK_RETURN(cudaDeviceSynchronize());
-
-    kernel_init_histogram<<<1, M>>>();
-    CUDA_CHECK_RETURN(cudaGetLastError());
-    CUDA_CHECK_RETURN(cudaDeviceSynchronize());
-
-    histogram<<<blocksPerGrid, threadsPerBlock>>>(threadsPerBlock, blocksPerGrid);
-
-    CUDA_CHECK_RETURN(cudaGetLastError());
-
-    CUDA_CHECK_RETURN(cudaMemcpyFromSymbol(h_vector_H, vector_H, M * sizeof(int)));
-    int acum = 0;
-
-    std::cout << "\nHistograma:" << std::endl;
-    for (int j = 0; j < M; j++) {
-      std::cout << "\tH[" << j << "]: " << h_vector_H[j] << std::endl;
-      acum += h_vector_H[i];
-    }
-    std::cout << "Total: " << acum << std::endl;
-
-    CUDA_CHECK_RETURN(cudaFree(states));
-    CUDA_CHECK_RETURN(cudaEventRecord(stop, 0));
-    CUDA_CHECK_RETURN(cudaEventSynchronize(stop));
-
-    CUDA_CHECK_RETURN(cudaEventElapsedTime(&t_duration[i], start, stop));
-  }
-  
-  float t_max = 0, t_min = FLT_MAX, mean = 0;
-  for (int i = 0; i < REPETITIONS; i++) {
-    mean += t_duration[i];
-    if (t_duration[i] > t_max) {
-      t_max = t_duration[i];
-    }
-    if (t_duration[i] < t_min) {
-      t_min = t_duration[i];
-    }
+  // Inicializacion de los vectores con valores aleatorios
+  for (int i = 0; i < N; i++) {
+    h_vector[i] = (int)rand() / (int)RAND_MAX;
   }
 
-  std::cout << "\n\nSe han realizado " << REPETITIONS << " pruebas" << std::endl;
-  std::cout << "Obteniendo un tiempo medio de: " << mean / REPETITIONS << " ms" << std::endl;
-  std::cout << "Con un tiempo maximo de: " << t_max << " ms" << std::endl;
-  std::cout << "Con un tiempo minimo de: " << t_min << " ms" << std::endl;
+  // Inicializacion de los vectores en la GPU
+  int *d_vector;
+  int *d_histogram;
+  CUDA_CHECK_RETURN(cudaMalloc((void **)&d_vector, N * sizeof(int)));
+  CUDA_CHECK_RETURN(cudaMalloc((void **)&d_histogram, M * sizeof(int)));
+
+  // Bloques e hilos
+  int threadPerBlock = 256;
+  int blockPerGrid = (N + threadPerBlock - 1) / threadPerBlock;
+
+
 
   return 0;
 }
--- Modulo_2_Programacion_GPUs/histograma_1.cu
+++ Modulo_2_Programacion_GPUs/histograma_1.cu
@@ -1,107 +1,135 @@
-/**
- *
- * Universidad de La Laguna
- * Escuela Superior de Ingeniería y Tecnología
- * Grado en Ingeniería Informática
- * Asignatura: Arquitecturas Avanzadas y de Propósito Específico
- * Curso: 4º
- * @file histograma_1.cu
- * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
- * @brief Version 1 histograma en CUDA, crear tantos hilos como elementos de V
- * para que cada uno se encargue de ir al elemento que le corresponda en V e
- * incremente la caja correcta en el vector histograma H (posiblemente de forma
- * atómica).
- * @version 0.1
- *
- * Compilar y ejecutar con: nvcc histograma.cu -o histograma
- *
- * @copyright Copyright (c) 2023
- *
- */
-
-#include <cuda.h>
-#include <curand_kernel.h>
-#include <float.h>
-#include <stdlib.h>
+#include <cuda_runtime.h>
+#include <stdio.h>
 #include <time.h>
 
-#include <iostream>
+#define N 5000000
+#define M 8
 
-#define N 500000000  // Numero de valores de entrada
-#define M 8          // Tamaño del histograma
-
-// Repeticon de pruevas para calculo de media, max y min
-#define REPETICONES 10000
-#define SCALA 50  // Datos calculados en cada hilo
-
-__device__ int vector_V[N];  // Vector de datos de entrada
-__device__ int vector_H[M];  // Vector del histograma
-
-/**
- * @brief Funcion para la comprobación de errores de CUDA
- *
- */
-static void CheckCudaErrorAux(const char *, unsigned, const char *, cudaError_t);
-#define CUDA_CHECK_RETURN(value) CheckCudaErrorAux(__FILE__, __LINE__, #value, value)
-
-
-/**
- * @brief Kernel para la inicializacion del vector de datos de entrada
- *
- */
-__global__ void inicializacion_vector(int random, curandState *states,
-                                      int threadsPerBlock, int blocksPerGrid) {
-  int iteraciones = SCALA;
-  if (blocksPerGrid - 1 == blockIdx.x && threadIdx.x == threadsPerBlock - 1) {
-    iteraciones = iteraciones + (N % SCALA);
+// Kernel de operaciones dentro del histograma
+__global__ void kernel(int *vector, int *histograma) {
+  int i = threadIdx.x + blockIdx.x * blockDim.x;
+  int posicion_histograma = 0;
+  if (i < N) {
+    posicion_histograma = vector[i] % M;  // ValorElementoV mod M
+    atomicAdd(&(histograma[posicion_histograma]), 1);
   }
-  unsigned id_x = blockIdx.x * blockDim.x + threadIdx.x;
-  curandState *state = states + id_x;
+}
 
-  curand_init(random, id_x, 0, state);
-  for (int i = 0; i < iteraciones; i++) {
-    if (id_x * SCALA + i < N) {
-      vector_V[id_x * SCALA + i] = (int)((curand_uniform(state) * 1000)) % M;
+// Kernel de inicialización del histograma
+__global__ void histograma(int *histo) {
+  int i = threadIdx.x + blockIdx.x * blockDim.x;
+  if (i < N) {
+    if (i == 0) {
+      for (int j = 0; j < M; j++) histo[j] = 0;
     }
   }
 }
 
-/**
- * @brief Kernel para la inicializacion del vector del histograma
- *
- */
-__global__ void inicializacion_histograma() {
-  int id_x = blockIdx.x * blockDim.x + threadIdx.x;
-  if (id_x < M) {
-    vector_H[id_x] = 0;
-  }
+// Función para mostrar histograma final
+void mostrar_histograma(int *hst_vector, int suma) {
+  printf("\n\t\t\t\t\tHISTOGRAMA\n\n");
+  printf(
+      "\n======================================================================"
+      "===============================\n");
+  for (int i = 1; i <= M; i++) printf("|    %d   | ", i);
+  printf("|   TOTAL  | ");
+  printf("\n");
+  for (int x = 0; x < M; x++) printf("| %d | ", hst_vector[x]);
+  printf("| %d  |", suma);
+
+  printf(
+      "\n======================================================================"
+      "===============================");
 }
+// rutina principal
 
-__global__ void histograma(int threadsPerBlock, int blocksPerGrid) {
-  int iteraciones = SCALA;
-  if (blocksPerGrid - 1 == blockIdx.x && threadIdx.x == threadsPerBlock - 1) {
-    iteraciones = iteraciones + (N % SCALA);
+int main(void) {
+  // vectores de números en host y device
+  int *hst_vector = (int *)malloc(N * sizeof(int)); // reservamos memoria en host vector
+  int *dev_vector = (int *)malloc(M * sizeof(int)); // reservamos memoria en host histograma
+  // vectores de histograma en host y device
+  int *hst_histograma;
+  int *dev_histograma;
+  // errores Cuda
+  cudaError_t error = cudaSuccess;
+  // bloques e hilos
+  int threadperBlock;
+  int blockperGrid;
+  // temporizadores
+  cudaEvent_t start;
+  cudaEvent_t stop;
+  float elapsedTime;
+  // comprobación final
+  int sum_elements = 0;
+
+  printf("Info: Reservando memoria para los vectores\n");
+  // reservamos memoria en host vector
+  // hst_vector = (int *)malloc(N * sizeof(int));
+
+  // reservamos memoria en host histograma
+  // hst_histograma = (int *)malloc(M * sizeof(int));
+
+  // reservamos memoria en device de vector
+    //   error = cudaMalloc((void **)&dev_vector, N * sizeof(int));
+    cudaMalloc((void **)&dev_vector, N * sizeof(int));
+
+  // reservamos memoria en device de histograma
+//   error = cudaMalloc((void **)&dev_histograma, M * sizeof(int));
+    cudaMalloc((void **)&dev_histograma, M * sizeof(int));
+
+  // inicialización de valores del vector de numeros con valores entre 1 y 8
+  srand((int)time(NULL));
+  for (int i = 0; i < N; i++) {
+    hst_vector[i] = (int)(1 + rand() % (256 - 1));
   }
-  unsigned id_x = blockIdx.x * blockDim.x + threadIdx.x;
-  for (int i = 0; i < iteraciones; i++) {
-    if (id_x * SCALA + i < N) {
-      int mod = vector_V[id_x * SCALA + i] % M;
-      atomicAdd(&vector_H[mod], 1);
-    }
+
+  // Copiamos los elementos del vector hst_vector en el vector dev_vector
+  printf("Info: Copiando elementos de HOST -> DEVICE");
+  error = cudaMemcpy(dev_vector, hst_vector, N * sizeof(int), cudaMemcpyHostToDevice);
+ 
+  error = cudaMemcpy(dev_histograma, hst_histograma, M * sizeof(int),cudaMemcpyHostToDevice);
+
+  threadperBlock = 512;
+  blockperGrid = (N + threadperBlock - 1) / threadperBlock;
+  printf("Info: CUDA ejecutara %d hilos y %d bloques\n", threadperBlock, blockperGrid);
+
+  // Ejecutaremos kernel y temporizadores
+  printf("Info: Ejecutamos Kernel e iniciamos temporizadores\n");
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  // Iniciamos temporizador
+  printf("Info: Temporizador iniciado\n");
+  cudaEventRecord(start, 0);
+  histograma<<<blockperGrid, threadperBlock>>>(dev_histograma);
+  kernel<<<blockperGrid, threadperBlock>>>(dev_vector, dev_histograma);
+  cudaEventRecord(stop, 0);
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&elapsedTime, start, stop);
+//   error = cudaGetLastError();
+
+  printf("Info: Duración de la creación del histograma completo %.2f sec\n", elapsedTime);
+  cudaEventDestroy(start);
+  cudaEventDestroy(stop);
+
+  printf("Info: Copiamos elementos de vectores dev -> host\n");
+  error = cudaMemcpy(hst_vector, dev_vector, N * sizeof(int),cudaMemcpyDeviceToHost);
+  error = cudaMemcpy(hst_histograma, dev_histograma, M * sizeof(int), cudaMemcpyDeviceToHost);
+
+  // comprobamos que la suma de los elementos del histograma corresponde con el
+  // numero de elementos
+  for (int x = 0; x < M; x++) {
+    sum_elements += hst_histograma[x];
   }
-}
+  if (sum_elements != N) {  // si no es igual, la ejecucion es incorrecta
+    printf("Error, en el histograma hay %d elementos de 5 millones\n",
+           sum_elements);
+  } else {  // si es igual, la ejecucion es correcta y mostramos el histograma
+    printf("\n\nInfo: Ejecución finalizada correctamente :) \n");
+    mostrar_histograma(hst_histograma, sum_elements);
+  }
 
-int main() {}
-
-
-/**
- * Check the return value of the CUDA runtime API call and exit
- * the application if the call has failed.
- */
-static void CheckCudaErrorAux (const char *file, unsigned line, const char *statement, cudaError_t err) {
-
-	if (err == cudaSuccess)
-		return;
-	std::cerr << statement<<" returned " << cudaGetErrorString(err) << "("<<err<< ") at "<<file<<":"<<line << std::endl;
-	exit (EXIT_FAILURE);
+  error = cudaFree(dev_vector);
+  error = cudaFree(dev_histograma);
+ 
+  return 0;
 }
